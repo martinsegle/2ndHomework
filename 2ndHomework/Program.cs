5b40dc27a78daa6ce72d8d76a4554722649a86a3
martinsegle <mr.martins.egle@gmail.com>
2022-06-21 00:08:35 +0300

    8 uzd

--- 2ndHomework/Program.cs
+++ 2ndHomework/Program.cs
@@ -1,42 +1,137 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.OutputEncoding = System.Text.Encoding.UTF8;
+//Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-// 1.uzdevums
+//Console.WriteLine();
+//Console.WriteLine("1.uzdevums");
+//Console.WriteLine();
 
-Console.WriteLine("Sveiks, kā Tevi sauc?");
-string userName = Console.ReadLine();
+//Console.WriteLine("Sveiks, kā Tevi sauc?");
+//string userName = Console.ReadLine();
 
-Console.WriteLine($"Sveiks, {userName}!");
+//Console.WriteLine($"Sveiks, {userName}!");
+
+//Console.WriteLine();
+//Console.WriteLine("2.uzdevums");
+//Console.WriteLine();
+
+//Console.WriteLine("Cik Tev ir gadu?");
+//string userAgeText = Console.ReadLine();
+
+//int userAge = int.Parse(userAgeText);
+
+//int nextYear = userAge + 1;
+
+//string adult = String.Format("Tu esi pilngadīgs");
+
+//if (userAge >= 18)
+//{
+//    Console.WriteLine($"Nākamgad Tev paliks {nextYear}. {adult}.");
+//}
+//else
+//{
+//    Console.WriteLine($"Nākamgad Tev paliks {nextYear}.");
+//}
+
+
+//Console.WriteLine();
+//Console.WriteLine("3.uzdevums");
+//Console.WriteLine();
+
+//Console.WriteLine("Ievadi skaitli");
+//string num1Text = Console.ReadLine();
+//double num1 = double.Parse(num1Text);
+
+//Console.WriteLine("Ievadi skaitli");
+//string num2Text = Console.ReadLine();
+//double num2 = double.Parse(num2Text);
+
+//Console.WriteLine("Ievadi skaitli");
+//string num3Text = Console.ReadLine();
+//double num3 = double.Parse(num3Text);
+
+//Console.WriteLine("Ievadi skaitli");
+//string num4Text = Console.ReadLine();
+//double num4 = double.Parse(num4Text);
+
+
+//double firstLargest = Math.Max(num1, num2);
+//double secondLargest = Math.Max(num3, num4);
+//double absolutLargest = Math.Max(firstLargest, secondLargest);
+
+
+//Console.WriteLine($"Lielākais Tevis ievadītais skaitlis ir {absolutLargest}");
+
+//Console.WriteLine();
+//Console.WriteLine("4.uzdevums");
+//Console.WriteLine();
+
+//double firstSmallest = Math.Min(num1, num2);
+//double secondSmallest = Math.Min(num3, num4);
+//double absolutSmallest = Math.Min(firstSmallest, secondSmallest);
+
+//Console.WriteLine($"Mazākais Tevis ievadītais skaitlis ir {absolutSmallest}");
+
+//Console.WriteLine();
+//Console.WriteLine("5.uzdevums");
+//Console.WriteLine();
+
+//double leftAfter = num1 % num2;
+
+//Console.WriteLine($"Dalījuma atlikums pirmajiem diviem ievadītajiem skaitļiem ir {leftAfter}");
+
+//Console.WriteLine();
+//Console.WriteLine("6.uzdevums");
+//Console.WriteLine();
+
+//Console.WriteLine("Ievadi skaitli");
+//string num5Text = Console.ReadLine();
+//double num5 = double.Parse(num5Text);
+
+//bool isEven = num5 % 2 == 0;
+
+//Console.WriteLine($"Skaitlis ir pāra skaitlis: {isEven}");
+
+//if (num5 % 2 == 0)
+//{
+//    Console.WriteLine("Skaitlis ir pāra skaitlis");
+//}
+//else
+//{
+//    Console.WriteLine("Skaitlis ir nepāra skaitlis");
+//}
+
+//Console.WriteLine();
+//Console.WriteLine("7.uzdevums");
+//Console.WriteLine();
+
+//Console.WriteLine("Ievadi taisnstūra a malas garumu");
+//string a_malaText = Console.ReadLine();
+//decimal a_mala = decimal.Parse(a_malaText);
+
+//Console.WriteLine("Ievadi taisnstūra b malas garumu");
+//string b_malaText = Console.ReadLine();
+//decimal b_mala = decimal.Parse(b_malaText);
+
+//decimal laukums = a_mala * b_mala;
+
+//Console.WriteLine($"Taisnstūra laukums ir {laukums}");
+
+//Console.WriteLine();
+//Console.WriteLine("8.uzdevums");
+//Console.WriteLine();
+
+//Console.WriteLine("Ievadi taisnleņķa vienādsānu trijstūra malas garumu");
+//double mala = double.Parse(Console.ReadLine());
+
+//double trijStLaukums = mala * mala / 2;
+
+//Console.WriteLine($"Trijstrūra laukums ir {trijStLaukums}");
 
 Console.WriteLine();
-Console.WriteLine("=======================");
+Console.WriteLine("9.uzdevums");
 Console.WriteLine();
 
-// 2.uzdevums
 
-Console.WriteLine("Cik Tev ir gadu?");
-string userAgeText = Console.ReadLine();
 
-int userAge = int.Parse(userAgeText);
-
-int nextYear = userAge + 1;
-
-string adult = String.Format("Tu esi pilngadīgs");
-
-if (userAge >= 18)
-{
-    Console.WriteLine($"Nākamgad Tev paliks {nextYear}. {adult}.");
-}
-else
-{
-    Console.WriteLine($"Nākamgad Tev paliks {nextYear}.");
-}
-
-     
-Console.WriteLine();
-Console.WriteLine("=======================");
-Console.WriteLine();
-
-// 3.uzdevums
 
